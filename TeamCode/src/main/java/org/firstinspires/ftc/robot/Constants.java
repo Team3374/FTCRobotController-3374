0f07181db04429a35d24f0a82f0060ee091439d9
Peyton Harris <95325343+peyhar@users.noreply.github.com>
2022-09-20 16:20:53 -0600

    Updated for 2022-2023

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/robot/Constants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.robot;
+
+public class Constants {
+    public final static class ExampleConstants {
+        public static final int EXAMPLE_NUMBER = 50;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/robot/Ports.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.robot;
+
+public class Ports {
+    public static final String EXAMPLE_MOTOR = "exampleMotor";
+}
